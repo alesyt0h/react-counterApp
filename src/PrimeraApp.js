059f68068b18e22196a35e44139f2b197e9ae409
Alex Ortigosa <alexortigosa179@gmail.com>
2021-10-30 04:49:30 +0200

    Fin sección 4

---
+++ src/PrimeraApp.js
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+// FC = Functional Component --- 
+const PrimeraApp = ({saludo, subtitulo}) => {
+
+    // const saludo = 'Hola Mundo';
+
+    return (
+        <>
+            <h1>{saludo}</h1>
+            {/* <pre>{JSON.stringify(saludo, null, 3)}</pre> */}
+            <p>{subtitulo}</p>
+        </>
+    );
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Mi primera aplicación'
+}
+
+
+export default PrimeraApp;
